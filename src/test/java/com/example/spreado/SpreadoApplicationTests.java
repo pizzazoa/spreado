0f2a543c4ff541d11ac781a21cc6478bcc11888f
pizzazoa <gkswns0716@naver.com>
2025-10-07 12:35:18 +0900

    feat(chore): 초기 세팅

---
+++ src/test/java/com/example/spreado/SpreadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spreado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
