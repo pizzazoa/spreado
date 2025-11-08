65fc56b485f2f1a5babf5eafcebb1307623876d9
1Seob <moon27456@naver.com>
2025-11-08 20:35:31 +0900

    fix(auth): 액세스 토큰 재발급 오류 수정
    JWT 필터를 시큐리티 체인에 등록

--- src/main/java/com/example/spreado/global/security/config/SecurityConfig.java
+++ src/main/java/com/example/spreado/global/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -53,6 +54,7 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(oAuth2SuccessHandler))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                         .build();
     }
 
