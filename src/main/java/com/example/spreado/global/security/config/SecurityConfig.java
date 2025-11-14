23e8a1fcc453a8c447c20c485b64e238bd72950e
pizzazoa <gkswns0716@naver.com>
2025-11-14 19:24:32 +0900

    feat(infra): 프론트 테스트를 위해 3000번대 포트도 허용 추가

--- src/main/java/com/example/spreado/global/security/config/SecurityConfig.java
+++ src/main/java/com/example/spreado/global/security/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8080"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
